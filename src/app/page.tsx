3436cb2854816326c78d733424deaa3f6543baa1
aachcoder47 <shivanshdata456@gmail.com>
2026-02-27 02:36:34 +1200

    Integrate Google Ads script into Home component
    Added Google Ads script for ad integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <main style={{ position: 'relative', overflowX: 'hidden' }}>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8490513657943266"
+     crossorigin="anonymous"></script>
       <div className="bg-dot-grid" />
       <BackgroundMesh />
 
